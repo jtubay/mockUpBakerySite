37bb6cc7963d1eb4cf897b911f8d7d04949be1c1
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-30 22:49:26 -0400

    added cart functionality

--- js/app.js
+++ js/app.js
@@ -21,8 +21,33 @@
                 const item = {};
                 item.img = `img-cart${partPath}`;
                 let name = e.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+                let price = e.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
                 item.name = name;
+                let finalPirce = price.slice(1).trim();
+                item.price = finalPirce;
                 console.log(item)
+
+                const cartItem = document.createElement('div');
+                cartItem.classList.add('cart-item', 'd-flex', 'justify-content-between', 'text-capitalize', 'my-3');
+
+                cartItem.innerHTML = `
+                    <img src="${item.img}" alt="" class="img-fluid rounded-circle" id="item-img">
+                    <div class="item-text">
+                        <p id="cart-item-title" class="font-weight-bold mb0">${item.name}</p>
+                        <span>$</span>
+                        <span id="cart-item-price" class="cart-item-price mb-0">${item.price}</span>
+                    </div>
+                    <a href="#" class="cart-item-remove">
+                        <i class="fas fa-trash"></i>
+                    </a>
+                </div>
+                `;
+            const cart = document.getElementById('cart');
+            const total = document.querySelector('.cart-total-container');
+
+            cart.insertBefore(cartItem, total);
+            alert('gang')
+
 
                 // console.log(e.target.dataset.goods);
 
